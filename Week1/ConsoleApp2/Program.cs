3b8d9bca4a1edc3c340d5305bf84afeb9e07f0a5
Muhammad Wali Ahmad <waliaslam2002@gmail.com>
2024-01-26 11:34:05 +0500

    Week 1 Tasks

---
+++ Week1/ConsoleApp2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student[] students = new Student[10];
+            for (int i = 0; i < 10; i++)
+            {
+                students[i] = new Student("Student" + i, i+10);
+            }
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(students[i].Name + " " + students[i].Grade);
+            }
+        }
+    }
+}
